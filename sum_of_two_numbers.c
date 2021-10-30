81058967f3b18a464a8d8c45eaee2fe1c231e96e
harshgupta49 <92860747+harshgupta49@users.noreply.github.com>
2021-10-30 17:47:19 +0530

    Create sum_of_two_numbers.c

---
+++ sum_of_two_numbers.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+int main()
+{
+    int x,y,z;
+    printf("please emter two digits you want to add\n");
+    scanf("%d%d",&x,&y);
+    z=x+y;
+    printf("Answer\t=%d",z);
+   
+}
+
+
+
+
