22b1b339d9b8a5058d1a07bbd9c9fdd1b938086e
harshgupta49 <92860747+harshgupta49@users.noreply.github.com>
2021-10-30 17:48:33 +0530

    Create add_sub_mul_div.c

---
+++ add_sub_mul_div.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+
+void main()
+{    int a,b,c;
+     printf("enter the value of a and b\n");
+     scanf("%d%d",&a,&b);
+     c=a+b;
+     printf("Addition\t=%d\n",c);
+     c=a-b;
+     printf("Subtract\t=%d\n",c);
+     c=a*b;
+     printf("Multiply\t=%d\n",c);
+     c=a/b;
+     printf("Divide\t\t=%d\n",c);
+}
