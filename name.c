7c1e4e4699c3c4c83abe063a2857a7d331b6e6bd
harshgupta49 <92860747+harshgupta49@users.noreply.github.com>
2021-10-30 17:46:12 +0530

    Create name.c

---
+++ name.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+
+void main()
+{
+    printf("Name\t\t:Harsh gupta\nSection\t\t:L1\nAddress\t\t:Sirsaganj(Firozabad)\nEmail id\t:harshgupt****@gmail.com\nPhone No\t:989752****");
+}
+
+
+
