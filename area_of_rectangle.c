e62fdbc82273fd09c01eff89b0aa9c5989eccb61
harshgupta49 <92860747+harshgupta49@users.noreply.github.com>
2021-10-31 13:23:26 +0530

    Create area_of_rectangle.c

---
+++ area_of_rectangle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{   int a,l,b;
+    printf("enter the value of length and breadth =");
+    scanf("%d%d",&l,&b);
+    a=l*b;
+    printf("Area =%d",a);
+    
+    return 0;
+}
