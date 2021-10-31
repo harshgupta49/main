054ac9794b5b7ccdc0254f2cc8fc8cc16ff5d57a
harshgupta49 <92860747+harshgupta49@users.noreply.github.com>
2021-10-31 13:17:07 +0530

    Create simple_interest.c

---
+++ simple_interest.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+     int p,r,t,SI;
+     printf("enter the value of p,r,t\n");
+     scanf("%d%d%d",&p,&r,&t);
+     SI=p*r*t/100;
+     printf("%d",SI);
+     
+     return 0;
+}
