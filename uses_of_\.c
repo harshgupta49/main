99fa56a02d943021234d973cf4f2b6eb3b4b2de6
harshgupta49 <92860747+harshgupta49@users.noreply.github.com>
2021-11-01 15:54:40 +0530

    Create uses_of_\.c

---
+++ uses_of_\.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("line change\n tab space\ttab space\n mr india \r invisible \n backz\bspace\n hello \vworld \naman \a cvraman");
+  
+
+    return 0;
+}
+
