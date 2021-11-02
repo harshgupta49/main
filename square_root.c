50f98858085ef9563c832e54444915556e9e4cf2
harshgupta49 <92860747+harshgupta49@users.noreply.github.com>
2021-11-02 18:24:26 +0530

    Create square_root.c

---
+++ square_root.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    float x,y;
+    printf("enter a number whose square root you want =");
+    scanf("%f",&x);
+    y=sqrt(x);
+    printf("root = %f",y);
+
+    return 0;
+}
+
