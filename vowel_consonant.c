2c29b4178a5a916f9193166e614189724350a7a7
harshgupta49 <92860747+harshgupta49@users.noreply.github.com>
2021-11-05 15:17:08 +0530

    Create vowel_consonant.c

---
+++ vowel_consonant.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+    char a;
+    printf("enter the alphabate =");
+    scanf("%c",&a);
+    switch(a){
+    case 97:
+    printf("vowel");
+    break;
+    case 101:
+    printf("vowel");
+    break;
+    case 105:
+    printf("vowel");
+    break;
+    case 111:
+    printf("vowel");
+    break;
+    case 117:
+    printf("vowel");
+    break;
+    default:
+    printf("consonant");
+    }
+
+    return 0;
+}
+
