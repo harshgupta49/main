e4e9d91b9e7b78953cd0990c2973ae82edff67bb
harshgupta49 <92860747+harshgupta49@users.noreply.github.com>
2021-11-10 11:24:30 +0530

    Update int_float_check.c

--- int_float_check.c
+++ int_float_check.c
@@ -2,16 +2,15 @@
 int main() 
 {
     float num,diffrence;
-    int int_num;
+    int a;
     printf("Enter a number :");
     scanf("%f",&num);
-    printf("int_num is %d\n",int_num);
-    diffrence=num-int_num;
-    if(diffrence==0){
-        printf("The number is int type.");
+    a=num;
+    if(num-a==0){
+        printf("Number is int type.");
     }
     else{
-        printf("The number if floting point.");
+        printf("Number is floting type.");
     }
     return 0;
 }
