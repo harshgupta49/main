4b9862dfc33cb09795ff5ce6d54a1c598ad4cb76
harshgupta49 <92860747+harshgupta49@users.noreply.github.com>
2021-11-11 23:07:15 +0530

    Create eligiblity_check_for_vote.c

---
+++ eligiblity_check_for_vote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int age;
+    printf("ENTER YOUR AGE = ");
+    scanf("%d",&age);
+    if(age>=18)
+    {
+        printf("you are eligible to vote");
+    }
+    else
+    {
+        printf("you are not eligible to vote");
+    }
+
+    return 0;
+}
