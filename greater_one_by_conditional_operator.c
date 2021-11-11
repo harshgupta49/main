4601511510c82749a1e6d135e9fee827daa41315
harshgupta49 <92860747+harshgupta49@users.noreply.github.com>
2021-11-11 16:57:59 +0530

    Create greater_one_by_conditional_operator.c

---
+++ greater_one_by_conditional_operator.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int n1,n2,m;
+    printf("Enetr the numbers = ");
+    scanf("%d %d",&n1,&n2);
+    m=(n1>n2)?n1:n2;
+    printf("Greator one =%d",m);
+    
+    return 0;
+}    
