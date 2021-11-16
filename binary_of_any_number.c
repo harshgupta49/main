8abb2659d839bb6d9145d3e2217f2f146846f8c1
harshgupta49 <92860747+harshgupta49@users.noreply.github.com>
2021-11-16 21:55:49 +0530

    Create binary_of_any_number.c

---
+++ binary_of_any_number.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include<math.h>
+int main()
+{
+    int num,i=16,a;
+    printf("Enter the number whopse binary you want :");
+    scanf("%d",&num);
+    while(num>0){
+        a=pow(2,i);
+        if(num/a>=1){
+            printf("1");
+            num=num-pow(2,i);
+        }
+        else{
+        printf("0");
+        }
+        i--;
+    }
+
+    return 0;
+}
