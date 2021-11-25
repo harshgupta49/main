cca03601f5cd5c1f2cd3a335ca1474aeecdb76c2
harshgupta49 <92860747+harshgupta49@users.noreply.github.com>
2021-11-26 00:07:44 +0530

    Update and rename celcius_into_fahrenhite.c to fahrenhite_to_celcius.c

---
+++ fahrenhite_to_celcius.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    float c,f;
+    printf("Enter the temperature in fahrenhite =");
+    scanf("%f",&f);
+    c=5.0/9*(f-32);
+    printf("Temperature in fahrenhite =%f",c);
+
+    return 0;
+}
