5ca810215f40109ba845f26311ed05ae6bd8a83f
harshgupta49 <92860747+harshgupta49@users.noreply.github.com>
2021-11-26 23:54:22 +0530

    Create voltage_calculate.c

---
+++ voltage_calculate.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    float a,b,c;
+    printf("Enter the value of current =");
+    scanf("%f",&a);
+    printf("Enter the value of resistance =");
+    scanf("%f",&b);
+    c=a*b;
+    printf("Voltage =%f",c);
+
+    return 0;
+}
