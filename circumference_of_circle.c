5be63447cf88c4ef231a15a71048b83e431f78db
harshgupta49 <92860747+harshgupta49@users.noreply.github.com>
2021-11-26 22:21:27 +0530

    Create circumference_of_circle.c

---
+++ circumference_of_circle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    float c,r;
+    printf("Enter the radius of thr circle =");
+    scanf("%f",&r);
+    c=3.14*2*r;
+    printf("Circumference =%f",c);
+
+    return 0;
+}
