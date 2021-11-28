3a38454408569ad4c75c450fee33605ce9fd5d49
harshgupta49 <92860747+harshgupta49@users.noreply.github.com>
2021-11-28 15:07:34 +0530

    Update armstring_series.c

--- armstring_series.c
+++ armstring_series.c
@@ -1,17 +1,23 @@
 #include <stdio.h>
-
+#include<math.h>
 int main()
 {
-    int num,i,r,sum=0,t;
+    int num,i,r,sum=0,t,x=0;
     printf("Enter the number = ");
     scanf("%d",&num);
     for(i=1;i<=num;i++){
         t=i;
         while(t>0){
+        x++;
+        t=t/10;
+        }
+        t=i;
+        while(t>0){
             r=t%10;
-            sum=sum+r*r*r;
+            sum=sum+pow(r,x);
             t=t/10;
         }
+        x=0;
         if(sum==i){
             printf("%d ",i);
         }
