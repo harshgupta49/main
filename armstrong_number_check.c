845bcfeae7a1a7ae1dd2d2e77861a861bb27d1c6
harshgupta49 <92860747+harshgupta49@users.noreply.github.com>
2021-11-28 14:15:19 +0530

    Update armstrong_number_check.c

--- armstrong_number_check.c
+++ armstrong_number_check.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
-
+#include<math.h>
 int main()
 {
-    int num,i,r,sum=0,t;
+    int num,i,r,sum=0,t,x=0;
     printf("Enter the number = ");
     scanf("%d",&num);
     t=num;
+    while(t>0){
+        x++;
+        t=t/10;
+    }
+    t=num;
     while(num>0){
         r=num%10;
-        sum=sum+r*r*r;
+        sum=sum+pow(r,x);
         num=num/10;
         }
     if(t==sum){
