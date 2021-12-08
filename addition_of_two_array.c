545bc702c87574fa51e795f40596d4c712987efb
harshgupta49 <92860747+harshgupta49@users.noreply.github.com>
2021-12-08 11:57:53 +0530

    Update addition_of_two_array.c

--- addition_of_two_array.c
+++ addition_of_two_array.c
@@ -2,14 +2,17 @@
 int main()
 {
 	int i,a[i],b[i],j,c[j],n;
-	printf("Enter the number of variable =");
+	printf("Enter the number of integers in these array =");
 	scanf("%d",&n);
+	printf("Enter the integers of first array =");
 	for(i=0;i<n;i++){
 		scanf("%d",&a[i]);
 	}
+	printf("Enter the integers of second array =");
 	for(i=0;i<n;i++){
 		scanf("%d",&b[i]);
 	}
+	printf("Sum of arrays =");
 	for(i=0;i<n;i++){
 		c[j]=a[i]+b[i];
 		printf("%d ",c[j]);
