99e93dba568fb0ed9054ea1d7eb37bbfae00a983
harshgupta49 <92860747+harshgupta49@users.noreply.github.com>
2022-01-02 16:52:02 +0530

    Create remove_last_occurance_of_character.c

---
+++ remove_last_occurance_of_character.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char str[100],n;
+    int i,x;
+    printf("Enter string :");
+    scanf("%[^\n]%*c",str);
+    printf("Enter the character :");
+    scanf("%c",&n);
+    for(i=0;str[i];i++){
+        if(str[i]==n){
+            x=i;
+        }
+    }
+    for(i=x;str[i];i++){
+        str[i]=str[i+1];
+    }
+    puts(str);
+
+    return 0;
+}
