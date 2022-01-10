98f2319fe098bfd6ad22c5050bdb3f062ce77a16
harshgupta49 <92860747+harshgupta49@users.noreply.github.com>
2022-01-10 22:41:06 +0530

    Create deletion_of_first_occurance_of_word.c

---
+++ deletion_of_first_occurance_of_word.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+    char str[100],word[100];
+    int i,j,x,y,f=0;
+    printf("Enter the string : ");
+    scanf("%[^\n]%*c",str);
+    printf("Enter the word :");
+    scanf("%[^\n]%*c",word);
+    x=strlen(str);
+    y=strlen(word);
+    for(i=0;i<=x-y;i++){
+        for(j=i;j<i+y;j++){
+            if(str[j]==word[j-i]){
+                f=1;
+            }
+            else{
+                f=0;
+                break;
+            }
+        }
+        if(f==1){
+            for(j=i;str[j];j++){
+                str[j]=str[j+y];
+            }
+            break;
+        }
+    }
+    printf("String after deletion of first occurances :");
+    puts(str);
+
+    return 0;
+}
